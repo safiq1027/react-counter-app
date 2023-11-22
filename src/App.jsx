fdc142f76472a05db536c1ecdd84d90d3e470529
safiq1027 <safiq1027@gmail.com>
2023-11-22 21:17:39 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import CounterApp from './components/CounterApp'
+
+export default function App() {
+  return (
+    <div>
+      <CounterApp/>
+      
+    </div>
+  )
+}
---
+++ src/components/CounterApp.jsx
@@ -0,0 +1,33 @@
+// eslint-disable-next-line no-unused-vars
+import React,{useState} from 'react';
+
+const Counter = () => {
+  const [count,setCount] = useState(0)
+
+ const handleIncrement = ()=>{
+  setCount((count)=>count+1)
+ }
+ const handleDecrement = ()=>{
+  setCount((count)=>count-1)
+ }
+ 
+ const reset=()=>{
+  setCount((count)=>count-count)
+ }
+
+  return (
+    <div className="counter center">
+      <h1 className="counter__title">Counter App</h1>
+      <div className="card center">
+        <h2 className="card__title">Count : {count}</h2>
+        <div className="card__btns">
+          <button onClick={handleIncrement} className="btn card__btn" disabled={count===5?true:false} >+</button>
+          <button onClick={handleDecrement} className="btn card__btn" disabled={count===-5?true:false}>-</button>
+          <button onClick={reset} className="btn card__btn">0</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
